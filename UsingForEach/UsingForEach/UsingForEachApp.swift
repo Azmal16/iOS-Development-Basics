19642f5555993933f6beab9e3a982d9e28c30397
Azmal Awasaf <aawsaf@mymedicalhub.com>
2022-12-23 17:59:52 +0600

    first commit

---
+++ UsingForEach/UsingForEach/UsingForEachApp.swift
@@ -0,0 +1,17 @@
+//
+//  UsingForEachApp.swift
+//  UsingForEach
+//
+//  Created by MMH on 23/12/22.
+//
+
+import SwiftUI
+
+@main
+struct UsingForEachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
