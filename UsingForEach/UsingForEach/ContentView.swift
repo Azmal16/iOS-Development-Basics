aef9ec3bec0c15ba54b2a77d477300b16e701734
Azmal Awasaf <aawsaf@mymedicalhub.com>
2022-12-27 18:08:06 +0600

    update foreach

--- UsingForEach/UsingForEach/ContentView.swift
+++ UsingForEach/UsingForEach/ContentView.swift
@@ -6,10 +6,15 @@ struct ContentView: View {
     @State private var selectedGoat = "Messi"
     var body: some View {
         NavigationView {
-            Form {
-                Picker("Select your GOAT", selection: $selectedGoat) {
-                    ForEach(goats, id: \.self) {
-                        Text($0)
+            VStack{
+                Text("Goat Selection Form")
+                Form {
+                    //selectedGoat is a two way binding
+                    Picker("Select your GOAT", selection: $selectedGoat) {
+                        //id is a way to identify each elements of an array uniquely. Here, each property of goats is unique
+                        ForEach(goats, id: \.self) { goat in
+                            Text(goat)
+                        }
                     }
                 }
             }
