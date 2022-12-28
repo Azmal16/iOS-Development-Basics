d72df3c385cbd3fc31f6d23b3ed9e1ba3e2fee15
Azmal Awasaf <aawsaf@mymedicalhub.com>
2022-12-28 20:52:57 +0600

    create stateproperties app

---
+++ StateProperties/StateProperties/ContentView.swift
@@ -0,0 +1,45 @@
+//
+//  ContentView.swift
+//  StateProperties
+//
+//  Created by MMH on 28/12/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State var name = ""
+    
+    var body: some View {
+        ZStack {
+            Color(UIColor(red: 0.14, green: 0.17, blue: 0.17, alpha: 1.00))
+            VStack{
+            
+            Text("Please Enter Your Name:")
+                    .font(.system(size: 20))
+                    .colorInvert()
+                    .padding(5)
+                TextField("Name", text: $name)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+               
+                if(name != "")
+                    {
+                    Text("Hello \(name)!")
+                        .font(.system(size: 20))
+                        .colorInvert()
+                }
+            
+        }
+        .padding()
+    }
+        .ignoresSafeArea()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StateProperties/StateProperties/StatePropertiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePropertiesApp.swift
+//  StateProperties
+//
+//  Created by MMH on 28/12/22.
+//
+
+import SwiftUI
+
+@main
+struct StatePropertiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
