1986e5c138c433ee337991ed46efb064e9d3b6df
Azmal Awasaf <aawsaf@mymedicalhub.com>
2022-12-29 13:41:15 +0600

    update state properties

--- StateProperties/README.md
+++ StateProperties/README.md
@@ -1,2 +1,15 @@
-## State Properties
-This App Shows how state properties and two way bindings work.
+# StateProperties
+An iOS app that greets you with your name
+## What It Does:
+
+This tiny app demonstrates the usage of state properties and two-way bindings. It takes a string as input and updates the UI according to the input string from the user. 
+
+## How does it look:
+![alt text](https://github.com/Azmal16/Images/blob/master/Statepropertiesdemo.gif "StateProperties UI Demo")
+
+## Concepts Used:
+
+* State properties
+* Two Way Bindings
+* Text Editor
+* UIColor
--- StateProperties/StateProperties/ContentView.swift
+++ StateProperties/StateProperties/ContentView.swift
@@ -9,31 +9,34 @@ import SwiftUI
 
 struct ContentView: View {
     
+    //UI gets changed accordig to the value of this property
     @State var name = ""
     
     var body: some View {
         ZStack {
             Color(UIColor(red: 0.14, green: 0.17, blue: 0.17, alpha: 1.00))
             VStack{
-            
-            Text("Please Enter Your Name:")
+                
+                Text("Please Enter Your Name:")
                     .font(.system(size: 20))
                     .colorInvert()
                     .padding(5)
+                //$ sign indicates two-way binding
+                //View code both reads and writes the value of "name"
                 TextField("Name", text: $name)
                     .textFieldStyle(RoundedBorderTextFieldStyle())
                     .padding()
-               
+                
                 if(name != "")
-                    {
+                {
                     Text("Hello \(name)!")
                         .font(.system(size: 20))
                         .colorInvert()
                 }
-            
+                
+            }
+            .padding()
         }
-        .padding()
-    }
         .ignoresSafeArea()
     }
 }
