3bb236b8229a653ee7b12cd19eacd60e57344d34
Azmal Awasaf <aawsaf@mymedicalhub.com>
2023-07-07 16:35:11 +0600

    add scrollview

--- CostCalculator/CostCalculator/ContentView.swift
+++ CostCalculator/CostCalculator/ContentView.swift
@@ -17,80 +17,82 @@ struct ContentView: View {
         ZStack {
             Color(UIColor(red: 0.14, green: 0.17, blue: 0.17, alpha: 1.00)).edgesIgnoringSafeArea(.all)
             
-            VStack(spacing: 25) {
-                
-                HStack {
-                    Image(systemName: "house")
-                        .font(.largeTitle.weight(.black))
-                    .foregroundColor(.white)
-                    VStack {
-                        Text("Cost")
-                            .foregroundColor(.white)
-                        .font(.title2)
-                        Text("Calculator")
-                            .foregroundColor(.white)
-                        .font(.title2)
-                    }
-                }
-                Text("Azmal")
-                    .foregroundColor(.white)
-                    .font(.title)
-                Group {
-                    TextField("Enter Grocery Cost", text: $azmalGrocery)
-                    TextField("Enter Unilty Cost", text: $azmalUtility)
-                }
-                .keyboardType(.numberPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-                
-                
-                Text("Mridul")
-                    .foregroundColor(.white)
-                    .font(.title)
-                
-                Group {
-                    TextField("Enter Grocery Cost", text: $mridulGrocery)
-                    TextField("Enter Unilty Cost", text: $mridulUtility)
-                }
-                .keyboardType(.numberPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-                
-                Text("Nasif")
-                    .foregroundColor(.white)
-                    .font(.title)
-                
-                Group {
-                    TextField("Enter Grocery Cost", text: $nasifGrocery)
-                    TextField("Enter Unilty Cost", text: $nasifUtility)
-                }
-                .keyboardType(.numberPad)
-                .textFieldStyle(RoundedBorderTextFieldStyle())
-                .padding(.horizontal)
-                
-                Button(action: {
-                    let perHeadGroceryCost = (Int(azmalGrocery)! + Int(mridulGrocery)! + Int(nasifGrocery)!) / 4
-                    let perHeadUtilityCost = (Int(azmalUtility)! + Int(mridulUtility)! + Int(nasifUtility)!) / 3
-                    azmalAccount = Int(azmalGrocery)! - perHeadGroceryCost + Int(azmalUtility)! - perHeadUtilityCost
-                    mridulAccount = Int(mridulGrocery)! - perHeadGroceryCost + Int(mridulUtility)! - perHeadUtilityCost
-                    nasifAccount = Int(nasifGrocery)! - perHeadGroceryCost + Int(nasifUtility)! - perHeadUtilityCost
-                    self.isCalculationComplete = true
-                }) {
-                    Text("Calculate")
+            ScrollView() {
+                VStack(spacing: 25) {
+                    
+                    HStack {
+                        Image(systemName: "house")
+                            .font(.largeTitle.weight(.black))
                         .foregroundColor(.white)
-                        .font(.headline)
-                        .padding()
-                        .background(Color.blue)
-                        .cornerRadius(20)
+                        VStack {
+                            Text("Cost")
+                                .foregroundColor(.white)
+                            .font(.title2)
+                            Text("Calculator")
+                                .foregroundColor(.white)
+                            .font(.title2)
+                        }
+                    }
+                    Text("Azmal")
+                        .foregroundColor(.white)
+                        .font(.title)
+                    Group {
+                        TextField("Enter Grocery Cost", text: $azmalGrocery)
+                        TextField("Enter Unilty Cost", text: $azmalUtility)
+                    }
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    
+                    
+                    Text("Mridul")
+                        .foregroundColor(.white)
+                        .font(.title)
+                    
+                    Group {
+                        TextField("Enter Grocery Cost", text: $mridulGrocery)
+                        TextField("Enter Unilty Cost", text: $mridulUtility)
+                    }
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    
+                    Text("Nasif")
+                        .foregroundColor(.white)
+                        .font(.title)
+                    
+                    Group {
+                        TextField("Enter Grocery Cost", text: $nasifGrocery)
+                        TextField("Enter Unilty Cost", text: $nasifUtility)
+                    }
+                    .keyboardType(.numberPad)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal)
+                    
+                    Button(action: {
+                        let perHeadGroceryCost = (Int(azmalGrocery)! + Int(mridulGrocery)! + Int(nasifGrocery)!) / 4
+                        let perHeadUtilityCost = (Int(azmalUtility)! + Int(mridulUtility)! + Int(nasifUtility)!) / 3
+                        azmalAccount = Int(azmalGrocery)! - perHeadGroceryCost + Int(azmalUtility)! - perHeadUtilityCost
+                        mridulAccount = Int(mridulGrocery)! - perHeadGroceryCost + Int(mridulUtility)! - perHeadUtilityCost
+                        nasifAccount = Int(nasifGrocery)! - perHeadGroceryCost + Int(nasifUtility)! - perHeadUtilityCost
+                        self.isCalculationComplete = true
+                    }) {
+                        Text("Calculate")
+                            .foregroundColor(.white)
+                            .font(.headline)
+                            .padding()
+                            .background(Color.blue)
+                            .cornerRadius(20)
+                    }
+                    Group {
+                        Text("Azmal: \(azmalAccount)")
+                        Text("Mridul: \(mridulAccount)")
+                        Text("Nasif: \(nasifAccount)")
+                    }.foregroundColor(isCalculationComplete ? .white :  Color(UIColor(red: 0.14, green: 0.17, blue: 0.17, alpha: 1.00)))
+                    Spacer()
                 }
-                Group {
-                    Text("Azmal: \(azmalAccount)")
-                    Text("Mridul: \(mridulAccount)")
-                    Text("Nasif: \(nasifAccount)")
-                }.foregroundColor(isCalculationComplete ? .white :  Color(UIColor(red: 0.14, green: 0.17, blue: 0.17, alpha: 1.00)))
-                Spacer()
+                .padding()
             }
-            .padding()
             
         }
     }
